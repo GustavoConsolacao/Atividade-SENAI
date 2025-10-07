12ecb1a281950ea52af3f4a4e189b01943e3ce67
Gustavo Consolação <gustavogcsantos66@gmail.com>
2025-10-06 21:57:11 -0300

    minha primeira atividade, atividade 12, com 6 questoes

---
+++ JavaTarde/src/main/java/Exemplos/Concatenar.java
@@ -0,0 +1,8 @@
+package Exemplos;
+
+public class Concatenar {
+    public static void main(String[] args) {
+        int x=10000;
+        System.out.println("O carro custa = " + x + " reais");
+    }
+}
