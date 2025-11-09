7a47832ff8b49219902e05ebcb204723a4025284
Gustavo Consolação <gustavogcsantos66@gmail.com>
2025-11-09 18:45:00 -0300

    atividade 12, a primeira, com 6 exercicios

---
+++ JavaTarde/src/test/java/application/Triangle.java
@@ -0,0 +1,22 @@
+package application;
+
+public class Triangle {
+    public double a;
+    public double b;
+    public double c;
+
+    public double area() {double p = (a + b + c) / 2.0;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+}
+    /*
+    A outra opcao era de se inserir o tipo de variavel por exemplo
+    double result = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    return result;
+
+    Essa maneira tbm funciona mas o melhor 'e o de cima por evitar quantidades enormes de codigos.
+     */
+
+
+
+
